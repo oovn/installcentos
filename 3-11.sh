f4572366773fe65dd128c1fa59d91331e2c7f670
oovn <41069034+oovn@users.noreply.github.com>
2018-12-20 01:19:23 +0700

    Update 3-11.sh

--- 3-11.sh
+++ 3-11.sh
@@ -93,7 +93,7 @@ cd openshift-ansible && git fetch && git checkout release-${VERSION} && cd ..
 cat <<EOD > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-${IP}		$(hostname) console console.${DOMAIN}  
+${IP}		$(hostname) ocp ocp.${DOMAIN}  
 EOD
 
 if [ -z $DISK ]; then 
